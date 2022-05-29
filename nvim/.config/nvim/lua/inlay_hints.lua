1f0ac2816f7c923225a90459b70d51ac446777eb
BATSCHiii <55348788+BATSCHiii@users.noreply.github.com>
2022-05-29 23:10:48 +0200

    Delete inlay_hints.lua

--- nvim/.config/nvim/lua/inlay_hints.lua
+++
@@ -1,4 +0,0 @@
-vim.cmd [[augroup ShowLineHints]]
-vim.cmd [[  au!]]
-vim.cmd [[  autocmd CursorHold,CursorHoldI,CursorMoved *.rs :lua require('lsp_extensions').inlay_hints{}]]
-vim.cmd [[augroup END]]
